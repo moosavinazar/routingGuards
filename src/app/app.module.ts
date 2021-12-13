b70b979e5ce393add03a2acbe69e45034d3680a1
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-13 18:34:50 +0330

    create project with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
